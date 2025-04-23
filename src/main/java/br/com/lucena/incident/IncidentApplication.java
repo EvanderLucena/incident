f7bcd95c6c27c7ffdf56dbf30e9da1fa6a04f216
EvanderLucena <evander.willian@gmail.com>
2025-04-23 16:04:51 -0300

    create project

---
+++ src/main/java/br/com/lucena/incident/IncidentApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucena.incident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentApplication.class, args);
+    }
+
+}
